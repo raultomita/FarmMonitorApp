f19a725bd04aab8ab172ccd344dae50f227b7459
Raul Tomita <raul.tomita@.gmail.com>
2017-12-14 07:52:21 +0200

    Initial commit

---
+++ app/src/main/java/raultomita/homeapp/MainActivity.java
@@ -0,0 +1,18 @@
+package raultomita.homeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
