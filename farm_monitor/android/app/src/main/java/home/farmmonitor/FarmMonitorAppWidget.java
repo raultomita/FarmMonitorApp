997c1c9636f6e60a3a8542b8e535b3879e7f1042
Raul Tomita <raul.tomita@RTOMITA>
2018-11-15 18:04:00 +0200

    Added first app widget

---
+++ farm_monitor/android/app/src/main/java/home/farmmonitor/FarmMonitorAppWidget.java
@@ -0,0 +1,39 @@
+package home.farmmonitor;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+
+public class FarmMonitorAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    private void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                 int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.farm_monitor_app_widget);
+
+        // Construct an Intent object includes web adresss.
+        Intent intent = new Intent(context, MainActivity.class);
+
+        // In widget we are not allowing to use intents as usually. We have to use PendingIntent instead of 'startActivity'
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        // Here the basic operations the remote view can do.
+        views.setOnClickPendingIntent(R.id.tvWidget, pendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
--- farm_monitor/lib/main.dart
+++ farm_monitor/lib/main.dart
@@ -1,10 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:connectivity/connectivity.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -12,9 +18,70 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: ConnectionStatus(),
         ),
       ),
     );
   }
+}
+
+class ConnectionStatusState extends State<ConnectionStatus>{
+  String _connectionStatus = 'Unknown';
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
+  @override
+  void initState(){
+    super.initState();
+    initConnectivity();
+    _connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((ConnectivityResult result) async {
+          if(result == ConnectivityResult.wifi){
+            final wifiname = await _connectivity.getWifiName();
+            setState(() => _connectionStatus = result.toString() + ": " + wifiname);
+          }
+          else{
+            setState(() => _connectionStatus = result.toString());
+          }
+
+        });
+  }
+
+  Future<void> initConnectivity() async {
+    String connectionStatus;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      connectionStatus = (await _connectivity.checkConnectivity()).toString();
+    } on PlatformException catch (e) {
+      print(e.toString());
+      connectionStatus = 'Failed to get connectivity.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _connectionStatus = connectionStatus;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(_connectionStatus);
+  }
+
+  @override
+  dispose() {
+    _connectivitySubscription.cancel();
+    super.dispose();
+  }
+}
+
+class ConnectionStatus extends StatefulWidget {
+  @override
+  ConnectionStatusState createState() => new ConnectionStatusState();
 }
